007709c90bffff6836f8b87cdb46e77eb4bd0d83
George Ciurescu <george.ciurescu@verint.com>
2022-10-13 16:41:15 +0100

    adding first scripts

---
+++ Intro/src/Section04/L03_LocatingByName.java
@@ -0,0 +1,23 @@
+package Section04;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class L03_LocatingByName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\gciurescu\\Downloads\\repos\\personal\\Selenium\\Sal Course\\Resources\\driver_v1\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		driver.get("https://automationtesting.co.uk/contactForm.html");
+		
+		driver.findElement(By.name("first_name")).sendKeys("abcd");
+		
+		
+	}
+	}
